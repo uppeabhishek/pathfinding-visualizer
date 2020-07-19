c986a62d4165be68acbd758d400dadab75e6110b
Abhishek <abhishek.uppe@ibigroup.com>
2020-07-20 00:00:32 +0530

    Added mouse events

--- src/components/graph/nodes.tsx
+++ src/components/graph/nodes.tsx
@@ -1,17 +1,70 @@
-import React, { FunctionComponent, useRef, useEffect } from "react";
+import React, { FunctionComponent, SyntheticEvent } from "react";
 import { useStyles } from "./styles";
 
 const Nodes: FunctionComponent<{height: number , width: number}> = ({height, width}) => {
     const classes = useStyles();
 
-    const rows = Math.round(height/25), cols = Math.round(width/25);
+    const trWidth = 30, trHeight = 30;
 
-    const Node = ()
+    const rows = Math.floor(height/trHeight), cols = Math.floor(width/trWidth);
+
+    function tdRowListener(e: SyntheticEvent<HTMLTableDataCellElement>) {
+        const currentTargetClassList = e.currentTarget.classList;
+        const classes = new Set(e.currentTarget.classList);
+        if (classes.has("selected")) {
+            currentTargetClassList.remove("selected");
+        }   
+        else {
+            currentTargetClassList.add("selected");
+        }
+    }
+
+    function onMouseMove(e: SyntheticEvent<HTMLTableDataCellElement>) {
+        tdRowListener(e);
+    }
+
+    function onClick(e: SyntheticEvent<HTMLTableDataCellElement>) {
+        tdRowListener(e);
+    }
+
+    function onMouseDown(e: SyntheticEvent<HTMLTableDataCellElement>) {
+        tdRowListener(e);
+        console.log(e.currentTarget);
+        // e.currentTarget.addEventListener('onmousemove', () => {
+        //     console.log(e);
+        //     onMouseMove(e);
+        // });
+    }
+
+    const ColNodes: FunctionComponent<{row: number}> = ({row}) => {
+        const res = [];
+        for (let i=0;i<cols; i++) {
+            res.push(
+                <td 
+                    data-id={`${row}${i}`}
+                    onClick={tdRowListener}
+                    onMouseDown={onMouseDown}
+                    key={`${row}${i}`}
+                    style={{width: trWidth, height: trHeight}}
+                />
+            );
+        }
+        return <>{res}</>;
+    }
+
+    const RowNodes: FunctionComponent = () => {
+        const res = [];
+        for (let i=0; i< rows; i++) {
+            res.push(<tr key={i}>{<ColNodes row={i}/>}</tr>);
+        }
+        return <>{res}</>;
+    };
+
     console.log(rows, cols);
     return (
-        <table>
+        <table className={classes.table}>
             <tbody>
-                
+               <RowNodes /> 
             </tbody>
         </table>
     );
--- src/components/graph/styles.ts
+++ src/components/graph/styles.ts
@@ -4,6 +4,15 @@ export const useStyles = makeStyles((theme) =>
     createStyles({
         root: {
             flex: 1
+        },
+        table: {
+            borderSpacing: 0,
+            "& td": {
+                border: "0.5px solid black"
+            },
+            "& td.selected": {
+                backgroundColor: "#72d5ad"
+            }
         }
     })
 );
