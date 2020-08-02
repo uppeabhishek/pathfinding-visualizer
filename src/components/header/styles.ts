1e2fe222dc4e7a0b01ab4988373fd846c05b2a77
Abhishek <abhishek.uppe@ibigroup.com>
2020-08-02 16:20:43 +0530

    UI changes

--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -4,16 +4,20 @@ export const useStyles = makeStyles((theme) =>
     createStyles({
         header: {
             backgroundColor: "cadetblue",
-            padding: "10px",
+            padding: "4px",
             maxWidth: "100%",
             overflowX: "auto"
         },
         toolbar: {
+            display: "flex",
+            flexWrap: "wrap",
+            alignItems: "center",
+            justifyContent: "space-between",
             "& > *": {
                 color: "white",
                 flexDirection: "column",
                 display: "flex",
-                marginRight: "40px"
+                margin: "5px 10px 5px 10px"
             }
         }
     })
